076c0c87d071d8a8a403ea0c0bda345a1d31fe7a
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-03-05 13:23:16 +0100

    forward name param

--- spring-demo-request/src/main/java/com/laszloborbely/spring_demo_request/controllers/DemoRequestController.java
+++ spring-demo-request/src/main/java/com/laszloborbely/spring_demo_request/controllers/DemoRequestController.java
@@ -2,6 +2,7 @@ package com.laszloborbely.spring_demo_request.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,9 +10,14 @@ import org.springframework.web.client.RestTemplate;
 public class DemoRequestController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String request() {
+    public String request(@RequestParam(name = "name", required = false) String name) {
         RestTemplate restTemplate = new RestTemplate();
         String requestURL = "http://localhost:8080/spring_demo_war_exploded/";
+
+        if (name != null) {
+            requestURL += "?name=" + name;
+        }
+
         return restTemplate.getForObject(requestURL, String.class);
     }
 }
