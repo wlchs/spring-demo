4ca38923d719631171db927c1c48f641251d32a7
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-03-05 13:02:52 +0100

    implement second rest service for sending requests to the first one

---
+++ spring-demo-request/src/main/java/com/laszloborbely/spring_demo_request/controllers/DemoRequestController.java
@@ -0,0 +1,17 @@
+package com.laszloborbely.spring_demo_request.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class DemoRequestController {
+
+    @RequestMapping(value = "/request", method = RequestMethod.GET)
+    public String request() {
+        RestTemplate restTemplate = new RestTemplate();
+        String requestURL = "http://localhost:8080/spring_demo_war_exploded/greeting";
+        return restTemplate.getForObject(requestURL, String.class);
+    }
+}
