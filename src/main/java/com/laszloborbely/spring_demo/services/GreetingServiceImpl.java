5a75a078ad5cc49f7a418398828fe7e9ff1023d8
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-03-05 11:11:43 +0100

    read greeting prefix from the application.properties file

--- src/main/java/com/laszloborbely/spring_demo/services/GreetingServiceImpl.java
+++ src/main/java/com/laszloborbely/spring_demo/services/GreetingServiceImpl.java
@@ -1,8 +1,14 @@
 package com.laszloborbely.spring_demo.services;
 
 public class GreetingServiceImpl implements GreetingService {
+    private String greetingPrefix;
+
     @Override
     public String greet(String name) {
-        return "Hello, " + name + "!";
+        return greetingPrefix + name;
+    }
+
+    public void setGreetingPrefix(String greetingPrefix) {
+        this.greetingPrefix = greetingPrefix;
     }
 }
