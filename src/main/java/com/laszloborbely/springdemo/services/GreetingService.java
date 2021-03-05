7aaf8df855bae67ba492ba8e65640114b9c2a02c
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2021-03-05 09:02:14 +0100

    reinitialize Spring MVC

--- src/main/java/com/laszloborbely/springdemo/services/GreetingService.java
+++
@@ -1,5 +0,0 @@
-package com.laszloborbely.springdemo.services;
-
-public interface GreetingService {
-    String greet(String name);
-}
--- src/main/java/com/laszloborbely/springdemo/services/GreetingServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.laszloborbely.springdemo.services;
-
-public class GreetingServiceImpl implements GreetingService {
-    @Override
-    public String greet(String name) {
-        return "Hello, " + name + "!";
-    }
-}
